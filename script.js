045ea1deccad5ef00713f7de16a8d4f3cd84a5b9
Lucca Boets <boetslucca@gmail.com>
2023-03-18 21:14:22 +0100

    add

--- script.js
+++ script.js
@@ -2,7 +2,89 @@ let count = 1500;
 let interval;
 let countdown = document.getElementById("countdown");
 let previousDuration = "pomodoro";
+let themes = [];
 // let count = 2;
+
+fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://pastebin.com/raw/kc0jj6gB')}`)
+    .then(response => {
+        if (response.ok) return response.json()
+        throw new Error('Network response was not ok.')
+    })
+    .then(data => {
+        console.log(JSON.parse(data.contents));
+        loadThemes(JSON.parse(data.contents));
+    });
+
+function loadThemes(data) {
+    let selection = document.getElementById("theme");
+
+    let count = 0;
+
+    selection.addEventListener("change", function () {
+        changeTheme(this.value);
+    });
+    
+    themes = data;
+
+    for (theme of data) {
+        let option = document.createElement("option");
+        option.value = theme.name;
+        option.textContent = theme.name;
+        selection.appendChild(option);
+
+        if (count == 0) {
+            changeTheme(theme.name);
+            selection.selected = true;
+        }
+
+        count++;
+    }
+
+    // var radios = document.getElementsByName("platform");
+    // console.log(radios)
+    // for (radio in radios) {
+    //     // console.log(radios[radio])
+    //     radios[radio].onclick = function () {
+    //         console.log(this.value)
+    //         changeTheme(this.value);
+    //     }
+    // }
+    
+    // const radios = document.querySelectorAll('input[type="radio"][name="platform"]');
+    // console.log(radios)
+    // radios.forEach(radio => {
+    //     radio.addEventListener("click", function () {
+    //         changeTheme(this.value);
+    //         if (this.checked) {
+    //             // Do something when the radio button is selected
+    //             console.log(`Radio button with value ${this.value} selected`);
+    //         }
+    //     });
+    // });
+
+    // document.body.innerHTML += style.outerHTML;
+    // console.log(style.innerHTML);
+}
+
+function changeTheme(name) {
+    // let theme = getCheckedValue("platform");
+    console.log(name);
+
+    let theme = themes.find(theme => theme.name == name);
+    console.log(theme);
+
+    document.body.style.backgroundImage = `url(${theme.backgroundUrl})`;
+}
+
+function getCheckedValue(groupName) {
+    var radios = document.getElementsByName(groupName);
+    for (i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            return radios[i].value;
+        }
+    }
+    return null;
+}
 
 function actionButton() {
     let button = document.getElementById("actionButton");
@@ -22,10 +104,10 @@ function actionButton() {
     }
 }
 
-function startTimer(){
-    
+function startTimer() {
+
     countdown.innerHTML = secondsToTime(count);
-    interval = setInterval(function() {
+    interval = setInterval(function () {
         count--;
         document.title = secondsToTime(count);
         countdown.innerHTML = secondsToTime(count);
@@ -36,12 +118,12 @@ function startTimer(){
     }, 1000);
 }
 
-function secondsToTime(seconds){
+function secondsToTime(seconds) {
     return new Date(seconds * 1000).toISOString().substring(14, 19)
 }
 
-function changeDuration(name){
-    console.log(name)  
+function changeDuration(name) {
+    console.log(name)
 
     clearInterval(interval);
 
@@ -52,11 +134,11 @@ function changeDuration(name){
     document.getElementById("actionButton").textContent = "start";
 
     if (name == "pomodoro") {
-        count = 25*60;
+        count = 25 * 60;
     } else if (name == "short") {
-        count = 5*60;
+        count = 5 * 60;
     } else if (name == "long") {
-        count = 10*60;
+        count = 10 * 60;
     }
 
     countdown.innerHTML = secondsToTime(count);
